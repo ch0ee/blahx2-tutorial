30763df817b0a83a5fe9c5c39196f822a2c9f135
ch0ee <choco920@gmail.com>
2023-04-17 00:03:16 +0900

    build 에러 수정

--- components/GNB.tsx
+++ components/GNB.tsx
@@ -1,6 +1,5 @@
 import { Avatar, Box, Button, Flex, IconButton, Menu, MenuButton, MenuItem, MenuList, Spacer } from '@chakra-ui/react';
 import { useAuth } from '@/contexts/auth_user.context';
-import MoreBtnIcon from './more_btn_icon';
 
 const GNB = function () {
   const { loading, authUser, signOut, signInWithGoogle } = useAuth();
